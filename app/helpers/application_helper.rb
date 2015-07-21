a107907cff265d7fb36388e85e028d1388965990
Daesun Yim <daesun@robinhood.com>
2015-07-20 19:29:26 -0700

    Full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns the full title
+  def full_title(page_title = '')
+    base_title = "Spider With 1000 Eyes"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
